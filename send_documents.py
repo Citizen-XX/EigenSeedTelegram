683f8c71306fc77f162bedd9b2407b0fc65e00a8
Citizen-XX <102715353+Citizen-XX@users.noreply.github.com>
2023-11-01 17:47:31 -0600

    BabaBooey path mejorado

--- send_documents.py
+++ send_documents.py
@@ -4,18 +4,18 @@ from telegram import InlineQueryResultArticle, InputTextMessageContent, InlineKe
 
 # Reemplaza 'TOKEN' con el token de tu bot de Telegram
 TOKEN = '6718203881:AAHglMpdnxv382DmXMwt1L8i4-gG6ere9ao'
-
 # Manejador para el comando /enviarexcel
 def send_excel(update, context):
     user_id = update.effective_user.id
-    doc_file = open('C:/Users/Fernando/OneDrive/Documentos/PythonProjects/Python/eigenseed_telegrambots/EigenSeedTelegram/BabaBooey.xlsx', 'rb')
-
+    doc_file = open('BabaBooey.xlsx', 'rb')
+    username = update.message.from_user.name
     # Reemplaza con la ruta de tu archivo Excel
     # excel_file_path = 'C:/Users/Fernando/OneDrive/Documentos/PythonProjects/Python/eigenseed_telegrambots/EigenSeedTelegram/BabaBooey.xlsx'
 
     # Envía el archivo de Excel al usuario
     # update.message.send_document(chat_id=user_id, document = excel_file_path)
     # update.message.reply_document(document=open(excel_file_path))
+    print(f"File sent to {username}")
     return context.bot.send_document(user_id, doc_file)
 
 def main():
